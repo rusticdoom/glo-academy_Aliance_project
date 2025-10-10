2079f8db7361565c462eaec2b13a9c27b6524c5f
Rusticdoom <ruslan.dostovalov@gmail.com>
2025-10-10 03:29:53 -0700

    fix: all svg files have been replaced with a sprite.svg

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,14 @@
 const navbar = () => {
   const navbar = document.querySelector(".navbar");
-  const logo_light = navbar.querySelector(".navbar .header-logo .logo-light");
-  const logo_main = navbar.querySelector(".navbar .header-logo .logo-main");
+  const logo = document.querySelector(".aliance_logo use");
 
   window.addEventListener("scroll", () => {
     if (this.scrollY > 1) {
       navbar.classList.add("navbar-light");
-      logo_light.style.display = "none";
-      logo_main.style.display = "inline";
+      logo.href.baseVal = "images/sprite.svg#aliance_logo_main";
     } else {
       navbar.classList.remove("navbar-light");
-      logo_light.style.display = "inline";
-      logo_main.style.display = "none";
+      logo.href.baseVal = "images/sprite.svg#aliance_logo_light";
     }
   });
 };
