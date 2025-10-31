a7c39c9e211a7f35319fadaafe07e90d80d7fa7f
Rusticdoom <ruslan.dostovalov@gmail.com>
2025-10-31 04:55:51 -0700

    add: js functions for openning and closing mobile navigation bar

--- js/navbar.js
+++ js/navbar.js
@@ -4,19 +4,39 @@ const navbar = () => {
   const mMenuToggle = document.querySelector(".mobile-menu-toggle");
   const menu = document.querySelector(".mobile-menu");
 
+  const lightModeOn = (event) => {
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "images/sprite.svg#aliance_logo_main";
+  };
+
+  const lightModeOff = (event) => {
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "images/sprite.svg#aliance_logo_light";
+  };
+
+  const openMenu = (event) => {
+    //openning the mobile menu
+    menu.classList.add("is-open");
+    mMenuToggle.classList.add("close-menu");
+    document.body.style.overflow = "hidden"; //turning off rolling of the page
+    lightModeOn();
+  };
+
+  const closeMenu = (event) => {
+    //closing the mobile menu
+    menu.classList.remove("is-open");
+    mMenuToggle.classList.remove("close-menu");
+    document.body.style.overflow = ""; //turning on rolling of the page
+    lightModeOff();
+  };
+
   window.addEventListener("scroll", () => {
-    if (this.scrollY > 1) {
-      navbar.classList.add("navbar-light");
-      logo.href.baseVal = "images/sprite.svg#aliance_logo_main";
-    } else {
-      navbar.classList.remove("navbar-light");
-      logo.href.baseVal = "images/sprite.svg#aliance_logo_light";
-    }
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
   });
 
   mMenuToggle.addEventListener("click", (event) => {
     event.preventDefault();
-    menu.classList.toggle("is-open");
+    menu.classList.contains("is-open") ? closeMenu() : openMenu();
   });
 };
 
