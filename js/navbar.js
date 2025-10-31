3566308957b1a51c7f6656eef4fc53a51059d568
Rusticdoom <ruslan.dostovalov@gmail.com>
2025-10-31 03:51:04 -0700

    add: top navigation bar mobile screen adaptivity layout (html, css)

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,8 @@
 const navbar = () => {
   const navbar = document.querySelector(".navbar");
   const logo = document.querySelector(".aliance_logo use");
+  const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+  const menu = document.querySelector(".mobile-menu");
 
   window.addEventListener("scroll", () => {
     if (this.scrollY > 1) {
@@ -10,6 +12,11 @@ const navbar = () => {
       navbar.classList.remove("navbar-light");
       logo.href.baseVal = "images/sprite.svg#aliance_logo_light";
     }
+  });
+
+  mMenuToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu.classList.toggle("is-open");
   });
 };
 
