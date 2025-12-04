0ef64ee3b5c4cf40ad841d9082d42e4b5bda3a6d
Rusticdoom <ruslan.dostovalov@gmail.com>
2025-12-04 03:16:32 -0800

    add: modal window "Having questions?" (html, css, js)

--- js/main.js
+++ js/main.js
@@ -73,3 +73,18 @@ const swiperBlog = new Swiper(".blog-slider", {
     },
   },
 });
+
+//modal
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll("[data-toggle=modal]");
+const modalClose = document.querySelector(".modal-close");
+modalToggle.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open");
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
